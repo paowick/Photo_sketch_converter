d45e4dcde2725b50be36255f69aa5865b53e4b58
paowick <korn2k9@gmail.com>
2023-03-16 06:50:05 +0700

    modified:   public/index.html 	new file:   public/script.js 	modified:   public/style.css

---
+++ public/script.js
@@ -0,0 +1,22 @@
+
+// Get the input element and the preview element
+const input = document.getElementById('photo');
+const preview = document.getElementById('preview');
+
+// Listen for changes to the input element
+input.addEventListener('change', function () {
+    // If a file is selected
+    if (input.files && input.files[0]) {
+        // Create a FileReader object
+        const reader = new FileReader();
+
+        // When the file is loaded
+        reader.addEventListener('load', function () {
+            // Set the preview image source to the loaded data URL
+            preview.src = reader.result;
+        });
+
+        // Read the file as a data URL
+        reader.readAsDataURL(input.files[0]);
+    }
+});
