6e520742274171bf7ac687adb3ed1584cb304f14
paowick <korn2k9@gmail.com>
2023-03-21 13:20:42 +0700

    deleted:    .gitignore 	deleted:    README.md 	deleted:    app.js 	new file:   img.jpg 	modified:   main.py 	deleted:    package-lock.json 	deleted:    package.json 	new file:   psc.exe 	deleted:    public/index.html 	deleted:    public/script.js 	deleted:    public/style.css 	deleted:    uploads/cat.jpg 	deleted:    uploads/cat.jpg.png

--- README.md
+++
@@ -1,6 +0,0 @@
-```
-$ pip install -r requirements.txt
-```
-```
-$ npm i
-```
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 def main(argv):
     img = cv2.imread(str(argv[0]),1)
+    paramcheck(str(argv[0]))
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img_invert = cv2.bitwise_not(img_gray)
     img_smoothing = cv2.GaussianBlur(img_invert,(21,21),sigmaX=0,sigmaY=0)
@@ -11,12 +12,18 @@ def main(argv):
     def dodgeV2(x,y):
         return cv2.divide(x,255-y,scale=256)
     final_img = dodgeV2(img_gray,img_smoothing)
-    cv2.imwrite(str(argv[0])+".png",final_img)
-
 
     
-    print(final_img)
-    sys.stdout.flush()
-        
+    cv2.imshow("final", final_img)
+    cv2.waitKey(0)
+
+
+def paramcheck(param):
+    text = """
+    hio
+    
+    """
+    if(param == None):
+        print(text)
 if __name__ == "__main__":
    main(sys.argv[1:])
