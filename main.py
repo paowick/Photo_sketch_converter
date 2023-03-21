e27887a99050f7dee96f19b56b721ca1cf9205cd
paowick <korn2k9@gmail.com>
2023-03-21 17:09:22 +0700

    .

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 
 def main(argv):
     img = cv2.imread(str(argv[0]),1)
-    paramcheck(str(argv[0]))
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img_invert = cv2.bitwise_not(img_gray)
     img_smoothing = cv2.GaussianBlur(img_invert,(21,21),sigmaX=0,sigmaY=0)
@@ -18,12 +17,5 @@ def main(argv):
     cv2.waitKey(0)
 
 
-def paramcheck(param):
-    text = """
-    hio
-    
-    """
-    if(param == None):
-        print(text)
 if __name__ == "__main__":
    main(sys.argv[1:])
