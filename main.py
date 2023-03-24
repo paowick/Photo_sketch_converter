d35588e0485c7ea24c12b634b167d2c5389bb1cf
paowick <korn2k9@gmail.com>
2023-03-24 12:05:41 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import cv2
 import sys
 
 def main(argv):
-    img = cv2.imread(str(argv[0]),1)
-    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    img_invert = cv2.bitwise_not(img_gray)
-    img_smoothing = cv2.GaussianBlur(img_invert,(21,21),sigmaX=0,sigmaY=0)
+    try:
+        img = cv2.imread(str(argv[0]),1)
+        img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img_invert = cv2.bitwise_not(img_gray)
+        img_smoothing = cv2.GaussianBlur(img_invert,(21,21),sigmaX=0,sigmaY=0)
 
 
-    def dodgeV2(x,y):
-        return cv2.divide(x,255-y,scale=256)
-    final_img = dodgeV2(img_gray,img_smoothing)
+        def dodgeV2(x,y):
+            return cv2.divide(x,255-y,scale=256)
+        final_img = dodgeV2(img_gray,img_smoothing)
 
 
-    if(final_img.shape[0] < 300 or final_img.shape[1] < 300):
-        final_img = cv2.resize(final_img,[500,500])
-        
-    cv2.imshow("final", final_img)
-    cv2.waitKey(0)
+        if(final_img.shape[0] < 300 or final_img.shape[1] < 300):
+            final_img = cv2.resize(final_img,[500,500])
 
+        cv2.imshow("final", final_img)
+        cv2.waitKey(0)
+    except:
+        print("         ------->  python main.py <image path>  <-------            ")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
